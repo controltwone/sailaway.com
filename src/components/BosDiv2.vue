c39531d99b842ae3db383ba79adf4344920f7aac
Ferhat Oguncer <96962387+controltwone@users.noreply.github.com>
2024-01-24 07:44:02 +0300

    Add files via upload

---
+++ src/components/BosDiv2.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="background-color: white; width: 1084px; height:2040px; margin-left: 215px; margin-top: 20px;">
+
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BosDiv3.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex-container">
+  <div class="flex-item"></div>
+  <div class="flex-item"></div>
+  <div class="flex-item"></div>
+</div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+ .flex-container {
+  display: flex;
+  justify-content: space-between; /* Kareler arasında boşluk bırakır */
+  margin-right: 304px;
+  margin-left: 220px;
+  margin-bottom: 30px;
+  margin-top: 30px;
+  height: 350px;
+
+}
+
+.flex-item {
+  width: calc(33.33% - 20px); /* Eşit genişlikte kareler, 10px boşluk bırakır */
+  background-color: white;
+  text-align: center;
+  padding: 20px;
+  box-sizing: border-box;
+
+}
+</style>
