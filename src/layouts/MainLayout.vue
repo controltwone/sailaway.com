42aec753d811c2d888813e176145ffcea67cc4c0
Ferhat Oguncer <96962387+controltwone@users.noreply.github.com>
2024-01-24 07:40:11 +0300

    Update MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,13 +8,14 @@
           <img src="https://c.wallhere.com/photos/ea/5a/windsurfing_sport_sea_Sun-8569.jpg!d" alt="Header Image" />
           <div class="header-text"><strong>Ferhat Oğuncer - 210101012</strong></div>
         </div>
-        <q-item-label header>Componentlerim</q-item-label>
+        <q-item-label header>Pages</q-item-label>
         <component-link
           v-for="link in componentLinks"
           :key="link.title"
           v-bind="link"
         />
       </q-list>
+
     </q-drawer>
 
     <q-page-container>
@@ -30,67 +31,33 @@ import ComponentLink, {
 } from 'components/ComponentLink.vue';
 
 const componentLinks: ComponentLinkProps[] = [
-{
-    title: 'TheHeader-1',
-    link: '/TheHeader',
-  },
 
   {
-    title: 'TheHeaderTop - 1.1',
-    link: '/TheHeaderTop',
+    title: 'Main Page',
+    link: '/MainPage',
   },
   {
-    title: 'TheHeaderMiddle - 1.2',
-    link: '/TheHeaderMiddle',
+    title: 'YeniUyelik Page',
+    link: '/UyelikPage',
   },
   {
-    title: 'TheHeaderBottom - 1.3',
-    link: '/TheHeaderBottom',
+    title: 'GirisYap Page',
+    link: '/GirisYapPage',
   },
   {
-    title: 'YeniUyelik - 2',
-    link: '/YeniUyelik',
+    title: 'Sepet Page',
+    link: '/SepetPage',
   },
   {
-    title: 'GirisYap - 3',
-    link: '/GirisYap',
+    title: 'Blog Page',
+    link: '/BlogPage',
   },
   {
-    title: 'TheSepet - 4',
-    link: '/TheSepet',
+    title: 'DetayliArama Page',
+    link: '/DetayliAramaPage',
   },
-  {
-    title: 'TheMarkalar - 5',
-    link: '/TheMarkalar',
-  },
-  {
-    title: 'TheCarusel - 6',
-    link: '/TheCarusel',
-  },
-  {
-    title: 'TheVitrindekiler - 7',
-    link: '/TheVitrindekiler',
-  },
-  {
-    title: 'DetaylıArama - 8',
-    link: '/DetaylıArama',
-  },
-  {
-    title: 'TumBloglar - 9',
-    link: '/TumBloglar',
-  },
-  {
-    title: 'TheFooter - 10',
-    link: '/TheFooter',
-  },
-  {
-    title: 'TheFooterTop - 10.1',
-    link: '/TheFooterTop',
-  },
-  {
-    title: 'TheFooterBottom - 10.2',
-    link: '/TheFooterBottom',
-  },
+
+
 ];
 
 const leftDrawerOpen = ref(false);
